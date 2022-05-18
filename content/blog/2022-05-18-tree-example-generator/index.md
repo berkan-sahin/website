6a33388ee059a72738d80e3161a9aec784fa5069
Berkan Şahin <berkan@bsahin.xyz>
2022-05-18 18:57:01 +0300

    add tree example generator

---
+++ content/blog/2022-05-18-tree-example-generator/index.md
@@ -0,0 +1,10 @@
+---
+title: "Tree Example Generator"
+date: 2022-05-18T18:37:36+03:00
+draft: false
+---
+
+I made a tool for generating random operation sequences (insertion/removal) for trees (2-3-4 tree, Red-Black tree, AVL etc.). You can access it from [https://bsahin.xyz/misc/tree](/misc/tree)
+
+Note to self: I should get around forking `hugo-theme-terminal` to accomodate for listing pages from multiple sections at some point (otherwise stuff not in `blog/` remains invisible).
+
---
+++ content/misc/tree/tree.js
@@ -0,0 +1,59 @@
+function randInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + Number(min);
+}
+
+function generateExample() {
+    var opList = document.getElementById("exampleOps");
+    document.getElementById("FZ").style.visibility = "hidden";
+    // Remove all children of the operation list
+    opList.replaceChildren();
+
+    // Get parameters
+    const minElem = document.getElementById("minElem").value;
+    const maxElem = document.getElementById("maxElem").value;
+    const allowRemoval = document.getElementById("allowRemove").checked;
+    var opCount = document.getElementById("opCount").value;
+
+    if (maxElem - minElem < 5) {
+        document.getElementById("FZ").style.visibility = "visible";
+        return;
+    } else if (opCount <= 0) {
+        alert("Operation count must be a positive integer.");
+        return;
+    }
+
+    var exampleOps = new Array();
+    var treeElems = new Set();
+
+    while (opCount--) {
+
+        if (allowRemoval &&
+            ((treeElems.size == maxElem - minElem + 1) || (treeElems.size >= 5 && Math.random() > 0.5))) {
+            // Delete item
+            const idx = randInRange(0, treeElems.size - 1);
+
+            // Temporarily transform the set into an array to delete a random element
+            var tmpArr = [...treeElems];
+            const deletedNumber = tmpArr[idx];
+            treeElems.delete(deletedNumber);
+
+            var operation = document.createElement("li");
+            operation.appendChild(document.createTextNode("Remove " + deletedNumber));
+            exampleOps.push(operation);
+        } else {
+            var newElem;
+            do {
+                newElem = randInRange(minElem, maxElem);
+            } while (treeElems.has(newElem));
+
+            treeElems.add(newElem);
+
+            var operation = document.createElement("li");
+            operation.appendChild(document.createTextNode("Insert " + newElem));
+            exampleOps.push(operation);
+        }
+    }
+
+    opList.replaceChildren(...exampleOps);
+
+}
